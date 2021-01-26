1c0439650802a3d3765f1939a9b5e7316a400262
SunEom <dkxl0701@gmail.com>
2021-01-26 16:40:51 +0900

    Go to google with selenium

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+
+
+
+browser.get('https://google.com')
