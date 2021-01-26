0224fd6aeb69ff3525d240ecf5b60078d920b3e1
SunEom <dkxl0701@gmail.com>
2021-01-26 16:48:49 +0900

    search keywords

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
-
-
 browser.get('https://google.com')
+
+search_bar = browser.find_element_by_class_name("gLFyf")
+
+search_bar.send_keys("hello!")
+search_bar.send_keys(Keys.ENTER)
+
+print(search_bar)
+
+# browser.quit()
