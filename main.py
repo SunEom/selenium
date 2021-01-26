6d380bd83e09c9ab564509d6433f4606f15bc40c
SunEom <dkxl0701@gmail.com>
2021-01-26 17:17:29 +0900

    wait for a something

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 
 KEYWORD = "buy domain"
 
@@ -13,12 +16,15 @@ search_bar = browser.find_element_by_class_name("gLFyf")
 search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
-search_results = browser.find_elements_by_class_name('g')
+shitty_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"g-blk")))
 
-for index, search_result in enumerate(search_results) :
-    class_name = search_result.get_attribute('class')
-    print(class_name)
-    if "kno-kp mnr-c g-blk" not in class_name:
-        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+print(shitty_element)
+# search_results = browser.find_elements_by_class_name('g')
 
-browser.quit()
+# for index, search_result in enumerate(search_results) :
+#     class_name = search_result.get_attribute('class')
+#     print(class_name)
+#     if "kno-kp mnr-c g-blk" not in class_name:
+#         search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+
+# browser.quit()
