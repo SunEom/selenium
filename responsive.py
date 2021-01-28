ebe8998f97c1a305fdb61668d0071e5a8fd15d24
SunEom <dkxl0701@gmail.com>
2021-01-28 17:33:10 +0900

    scroll down automatically

--- responsive.py
+++ responsive.py
@@ -1,6 +1,9 @@
 import time 
+from math import ceil
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+
+BROWSER_HEIGHT = 1056;
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -8,10 +11,14 @@ browser.get("https://nomadcoders.co");
 browser.maximize_window()
 
 
-sizes =[320, 480, 960, 1366, 1920]
-HEIGHT = 1056;
+sizes =[ 480, 960, 1366, 1920]
 print(browser.get_window_size())
 
 for size in sizes : 
-    browser.set_window_size(size,HEIGHT)
-    time.sleep(1)
+    browser.set_window_size(size,BROWSER_HEIGHT)
+    time.sleep(3)
+    scroll_size = browser.execute_script("return document.body.scrollHeight")
+    total_sections = ceil(scroll_size / BROWSER_HEIGHT)
+    for section in range(total_sections):
+        browser.execute_script(f"window.scrollTo(0,{(section+1) * BROWSER_HEIGHT})")
+        time.sleep(2)
