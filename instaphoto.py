430d2c7f36c1d830bf6c270980eed0e13c9ea2ec
SunEom <dkxl0701@gmail.com>
2021-01-29 14:11:48 +0900

    Refactor code

--- instaphoto.py
+++ instaphoto.py
@@ -1,4 +1,5 @@
 import time
+import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
@@ -6,36 +7,65 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-KEYWORD="아이유"
+class InstagramKeywordScreenshooter:
+    def __init__(self, keywords):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.keywords = keywords
+        
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+    def start(self):
+        self.login()
+        for keyword in self.keywords : 
+            self.search(keyword);
+            self.screenshot(keyword)
 
-browser.get('https://www.instagram.com/')
+    def login(self):
+        self.browser.get('https://www.instagram.com/')
 
-WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "HmktE")))
+        WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "HmktE")))
 
-insta_username = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
-insta_password = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+        insta_username = self.browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
+        insta_password = self.browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
 
-insta_username.send_keys(input("What is your Username"))
-insta_password.send_keys(input("What is your Password"))
-insta_password.send_keys(Keys.ENTER)
+        insta_username.send_keys(input("What is your Username ? "))
+        insta_password.send_keys(input("What is your Password ? "))
+        insta_password.send_keys(Keys.ENTER)
 
-WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "cmbtv")))
-browser.find_element_by_class_name("yWX7d").click()
-browser.find_element_by_class_name("HoLwm").click()
+        WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "cmbtv")))
+        self.browser.find_element_by_class_name("yWX7d").click()
+        self.browser.find_element_by_class_name("HoLwm").click()
 
-search_bar = browser.find_element_by_class_name("x3qfX")
-search_bar.send_keys(f"#{KEYWORD}")
-WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "yCE8d")))
-browser.find_element_by_class_name("yCE8d").click()
+    def search(self,keyword):
+        search_bar = self.browser.find_element_by_class_name("x3qfX")
+        search_bar.send_keys(f"#{keyword}")
+        WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "yCE8d")))
+        self.browser.find_element_by_class_name("yCE8d").click()
+        time.sleep(5)
+    
+    def is_dir(self, keyword):
+        if not os.path.exists("screenshots"):
+            os.makedirs("screenshots")
+            os.makedirs(f"screenshots/{keyword}")
 
-time.sleep(5)
-imgs = browser.find_elements_by_class_name("eLAPa")
+        elif not os.path.exists(f"screenshots/{keyword}"):
+            os.makedirs(f"screenshots/{keyword}")
 
-for index,img in enumerate(imgs) :
-    img.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+    def screenshot(self,keyword):
+        self.is_dir(keyword)
+        imgs = self.browser.find_elements_by_class_name("eLAPa")
+        for index,img in enumerate(imgs) :
+            img.screenshot(f"screenshots/{keyword}/{keyword}x{index+1}.png")
+
+    def finish(self):
+        time.sleep(3);
+        self.browser.quit()
+
+screenshooter = InstagramKeywordScreenshooter(["고양이","강아지"])
+screenshooter.start()
+screenshooter.finish()
+
+
+
+
     
 
-time.sleep(3);
-browser.quit()
