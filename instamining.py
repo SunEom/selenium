4e73151814e2851e4b5b6a60e9b24dd7046652fd
SunEom <dkxl0701@gmail.com>
2021-01-29 13:49:01 +0900

    Refactor code

--- instamining.py
+++ instamining.py
@@ -6,42 +6,57 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
-INSTA_ID="USERNAME"
-INSTA_PWD="PASSWORD"
-
-browser = webdriver.Chrome(ChromeDriverManager().install())
-
-main_hashtag = "정은지"
-
-browser.get(f"https://www.instagram.com/explore/tags/{main_hashtag}/");
-
-WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "_2hvTZ")))
-
-insta_id = browser.find_element_by_name("username");
-insta_pwd = browser.find_element_by_name("password");
-
-insta_id.send_keys(INSTA_ID)
-insta_pwd.send_keys(INSTA_PWD)
-insta_pwd.send_keys(Keys.ENTER)
-
-time.sleep(5)
-browser.find_element_by_class_name("yWX7d").click()
-browser.find_element_by_class_name("HoLwm").click()
-
-search_bar = browser.find_element_by_class_name("x3qfX")
-search_bar.send_keys(f"#{main_hashtag}")
-
-WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "yCE8d")))
-
-hashtags = browser.find_elements_by_class_name("_2_M76")
-results = [];
-for hashtag in hashtags : 
-    results.append({"title":  hashtag.find_element_by_class_name("Ap253").text, "number":  hashtag.find_element_by_class_name("Fy4o8").text.replace("게시물 ","")})
-
-for result in results : 
-    print(result)
 
 
+class InstagramHashtagMiner : 
+    def __init__(self,keyword):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.keyword = keyword
+        
 
-time.sleep(5);
-browser.quit()
+    def start(self):
+        self.login()
+        self.search()
+    
+    def finish(self):
+        time.sleep(5)
+        self.browser.quit()
+
+    def login(self):
+        self.browser.get(f"https://www.instagram.com/");
+        WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "_2hvTZ")))
+
+        insta_id = self.browser.find_element_by_name("username");
+        insta_pwd = self.browser.find_element_by_name("password");
+
+        insta_id.send_keys(input("What is your Username ? "))
+        insta_pwd.send_keys(input("What is your Password ? "))
+        insta_pwd.send_keys(Keys.ENTER)
+
+        time.sleep(5)
+        self.browser.find_element_by_class_name("yWX7d").click()
+        self.browser.find_element_by_class_name("HoLwm").click()
+
+    def search(self):
+        search_bar = self.browser.find_element_by_class_name("x3qfX")
+        search_bar.send_keys(f"#{self.keyword}")
+
+        WebDriverWait(self.browser, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "yCE8d")))
+
+        hashtags = self.browser.find_elements_by_class_name("_2_M76")
+        results = [];
+        for hashtag in hashtags : 
+            results.append({"title":  hashtag.find_element_by_class_name("Ap253").text, "number":  hashtag.find_element_by_class_name("Fy4o8").text.replace("게시물 ","")})
+        self.print_results(results)
+
+    def print_results(self,results) :
+        for result in results : 
+            print(result)
+
+
+
+dog_miner = InstagramHashtagMiner("강아지")
+dog_miner.start()
+dog_miner.finish()
+
+
